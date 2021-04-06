f224da29bd6893a620bbcfe4e76d351dd866c718
linweisen <linweisen@sandboxol.com>
2021-04-06 17:33:46 +0800

    定义数据解析接口

---
+++ crawler/src/main/java/top/interc/crawler/parser/ParseHandler.java
@@ -0,0 +1,13 @@
+package top.interc.crawler.parser;
+
+/**
+ * @author ：linweisen
+ * @date ：Created in 2021/4/6 17:29
+ * @description：${description}
+ * @modified By：
+ * @version: 1.0
+ */
+public interface ParseHandler {
+
+    ParseData parse(String raw, String contentType);
+}
